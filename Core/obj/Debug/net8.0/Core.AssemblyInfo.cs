afce014d5a6e8aa58bd32ed2ff4a7f3e10cd6a2b
Christian Colson <christiancolson91@gmail.com>
2025-01-03 14:29:48 -0600

    program file updated and database seeded

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef24a50c27c3f5aa1c4054d3fcabb697e3de1ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97156f157ca0adee772b10faf2f1bac48cfcdb55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
