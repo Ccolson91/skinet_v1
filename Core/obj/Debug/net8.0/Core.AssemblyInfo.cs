df5472b5a08573ae2249ba2204fa94ccc95233dd
Christian Colson <christiancolson91@gmail.com>
2025-01-21 10:16:55 -0600

    CORS support enabled

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf714fc3a431b59e427164065475334b4366e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28dfe89da2102175e0afce4308871fc6d1aaadd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
