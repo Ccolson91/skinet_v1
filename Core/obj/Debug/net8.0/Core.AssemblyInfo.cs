3fbdae71665a61ea6c8b6619e2185d39c679a7b9
Christian Colson <christiancolson91@gmail.com>
2025-01-23 15:23:37 -0600

    angular material tailwind added to project

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5472b5a08573ae2249ba2204fa94ccc95233dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0692baecf109edbd8ea3d022524e34aae59c72ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
