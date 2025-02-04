06588c71ac3c454037069384e13c1884331f4087
Christian Colson <christiancolson91@gmail.com>
2025-02-04 12:16:28 -0600

    set up routing/product details component created/nav links updated

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ShopComponent } from './features/shop/shop.component';
+import { ProductDetailsComponent } from './features/home/shop/product-details/product-details.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'shop', component: ShopComponent},
+    {path: 'shop/:id', component: ProductDetailsComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'},
+];
